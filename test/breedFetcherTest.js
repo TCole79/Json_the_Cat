67cd5f9e52767781b23f10c5f45bab6b0ddd536d
Tristan Cole <tristanleighcole@googlemail.com>
2021-12-03 04:26:54 +0000

    cat tester updated

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,36 @@
+// breedFetcherTest.js
+
+const { fetchBreedDescription } = require("../breedFetcher");
+const { assert } = require("chai");
+
+describe("fetchBreedDescription", () => {
+  it("returns a string description for a valid breed, via callback", (done) => {
+    fetchBreedDescription("Siberian", (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc =
+        "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+  it("returns null when an invalid/non-existent breed is entered, via callback", (done) => {
+    fetchBreedDescription("Siiberian", (err, desc) => {
+      // we expect null for this scenario
+      assert.exists(err); // changed this for new scenario
+
+      const expectedDesc =
+        "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(undefined, desc); // changed this for this scenario
+
+      done();
+    });
+  });
+});
